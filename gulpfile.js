433dfa41bd4f16bfc51793a7a4019d433da1b3ba
Aditya Nagla <me@cdadityang.xyz>
2020-03-28 08:32:26 +0000

    V1.0.2: Added gulp support and minor changes

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+let gulp = require('gulp');
+let clean = require('gulp-clean');
+let minifyCSS = require('gulp-clean-css');
+let uglify = require('gulp-uglify-es').default;
+let htmlmin = require('gulp-htmlmin');
+let autoprefixer = require('gulp-autoprefixer');
+let merge = require('merge-stream');
+
+// Location to our files
+var globs = {
+  build: './build',
+  css: './src/css/**/*.css',
+  js: './src/js/**/*.js',
+  html: './src/*.html',
+  images: './src/assets/**',
+  manifest: './src/manifest.json'
+};
+
+
+// First clean the build folder, it will delete it and recreate it
+gulp.task('clean', gulp.series(function() {
+  return gulp.src(globs.build, {read: false})
+    .pipe(clean());
+}));
+
+
+// Move images to /build folder
+// Move manifest.json to /build folder
+// You can also perform scaling and compression on images
+gulp.task('assets', gulp.series('clean', function() {
+  var images = gulp.src(globs.images).pipe(gulp.dest(globs.build + '/assets'));
+  var manifest = gulp.src(globs.manifest).pipe(gulp.dest(globs.build + '/'));
+
+  return merge(images);
+}));
+
+
+// Take all CSS in /css folder -> Add auto prefixer to them
+// Then Minify and export to /build folder
+gulp.task('styles', gulp.series('clean', function() {
+  return gulp.src(globs.css)
+    .pipe(autoprefixer())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(globs.build + '/css'));
+}));
+
+
+// Take all JS in /js folder
+// Then Minify and export to /build folder
+gulp.task('js', gulp.series('clean', function() {
+  return gulp.src(globs.js)
+    .pipe(uglify())
+    .pipe(gulp.dest(globs.build + '/js'));
+}));
+
+
+// Take the HTML file, minify it with params below
+gulp.task('html', gulp.series('clean', function() {
+  return gulp.src(globs.html)
+  .pipe(htmlmin({
+    collapseWhitespace: true,
+    removeComments: true
+  }))
+  .pipe(gulp.dest('./build'));
+}));
+
+
+// Build the task
+gulp.task('build', gulp.parallel('assets', 'styles', 'js', 'html'));
+
+gulp.task('default', gulp.series('build'));
